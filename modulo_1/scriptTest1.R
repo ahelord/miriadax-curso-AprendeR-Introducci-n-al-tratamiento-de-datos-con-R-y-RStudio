b4de865f062a8d15fae5fc99ba0403946565e9c5
Leonardo Rodriguez <lrodriguez@localhost.localdomain>
2016-11-29 23:24:30 -0500

    Se agregan ejercicios P2P de todo el curso

---
+++ modulo_1/scriptTest1.R
@@ -0,0 +1,10 @@
+#1 ninguna de las anteriores
+round((5*log(sqrt(2),3))/(((exp(1)^3)^(1/5))*7),4)
+#2 ninguna de las anteriores
+1.5^(-3)-(2^7/(7*sqrt(2)))*(2+(-1)^6)
+#3
+round(sin(37*(pi/180)),4)
+#4
+print(3.3333*10^10,10)
+#5
+round((sqrt(3)^exp(1)),5)
---
+++ modulo_1/scriptTest_optativo_Numeros_Complejos.R
@@ -0,0 +1,11 @@
+#1
+((2+3i)^2)/((5+1i)*(6-2i))
+round(Mod(-0.2+0.35i),2)
+#2
+x=complex(real = 1.5,imaginary = sqrt(2))
+xi=x^10
+round(Re(xi),3)
+#3
+z=complex(real = 3,imaginary = -(5/7))
+argumento=Arg(z)
+round((180/pi)*argumento,2)
